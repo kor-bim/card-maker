62d005dcec14402157fe52265858ab8965d19a29
kor-bim <dbsgksqlschl@gmail.com>
2022-06-05 21:42:45 +0900

    Add login component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.module.css';
-import App from './App';
+import App from './app';
+import {firebaseApp} from "./service/firebase";
+import AuthService from "./service/auth_service";
+
+const authService = new AuthService(firebaseApp);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App authService={authService}/>
+    </React.StrictMode>
 );
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,17 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCLhmQL8wA10z0tj1B7hB8T8DPgQc8Um3A",
-    authDomain: "business-card-maker-e939e.firebaseapp.com",
-    databaseURL: "https://business-card-maker-e939e-default-rtdb.firebaseio.com",
-    projectId: "business-card-maker-e939e",
-    storageBucket: "business-card-maker-e939e.appspot.com",
-    messagingSenderId: "879332318457",
-    appId: "1:879332318457:web:f83ca3a2875f8d079d9628",
-    measurementId: "G-GXVGJPBRL0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const firebaseApp = initializeApp(firebaseConfig);
