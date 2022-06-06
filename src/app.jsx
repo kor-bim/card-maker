549d75caaf580adf1a5e9f57230cbc0f9c18e142
kor-bim <dbsgksqlschl@gmail.com>
2022-06-06 21:35:40 +0900

    Add maker page

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,18 @@
 import styles from './app.module.css';
 import Login from "./components/login/login";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Maker from "./components/maker/maker";
 
 function App({authService}) {
     return (
         <div className={styles.app}>
-            <Login authService={authService}/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' exact element={<Login authService={authService}/>}/>
+                    <Route path='/maker' element={<Maker authService={authService}/>}/>
+                </Routes>
+            </BrowserRouter>
+
         </div>
     );
 }
